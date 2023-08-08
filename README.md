ac5e5c87a5139ff7fbf86eaf064761346c3302d3
Alina Kriutchenko <akriutchenko@deakin.edu.au>
2023-08-08 20:32:31 +1000

    commit 1

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # power-optimization-linear-regression
+
+# install:
+pip install pandas
+pip install numpy
+pip install matplotlib
+pip install scikit-learn
+
+# to run the code:
+python main.py
+
+# limitations:
+1. Linear Model Assumption: The linear regression models used may not capture potential nonlinear relationships between inputs and power consumption.
+2. Data Filtering Impact: Excluding data outside the 'check' range might limit the models' representativeness and generalizeability.
+3. Limited Generalization: Models might not perform well on new data or different conditions without cross-validation or external validation.
+4. Discrete Optimization: Optimization using discrete GPH values might not cover all potential solutions or constraints.
+5. Computational Complexity: The iterative optimization process could become resource-intensive for larger datasets or complex models.
+6. Single-Objective Focus: The optimization focused solely on minimizing power, overlooking potential trade-offs with other factors.
+7. Machine Interaction: The models treat machines as independent, ignoring potential interactions affecting power consumption.
+8. Model Complexity vs. Interpretability: Complex models might offer better accuracy but at the cost of interpretability.
+9. Real-World Feasibility: Optimized GPH values might not align with practical constraints or maintenance considerations.
+10. Unconsidered Factors: Factors like temperature, humidity, and maintenance schedules were omitted from the analysis.
+
+# conclusions:
+Optimal GPH Values:
+- After optimization, the optimal Goods Per Hour (GPH) values were determined to minimize power consumption while achieving the target total GPH of 9,000 for the factory.
+- Optimal GPH for Machine Type #1: 600
+- Optimal GPH for Machine Type #2: 300
+
+Total Power Consumption:
+- The total power consumption of the factory was calculated using the optimized GPH values.
+- Total Power Consumption Before Optimization: 1874.42
+- Total Power Consumption After Optimization: 1521.94
+
+Power Savings:
+- By implementing the optimized GPH values, the factory achieved a power savings of 352.49 units.
+
--- exercise.py
+++ exercise.py
@@ -1,80 +1,133 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_absolute_error, mean_squared_error
-from scipy.optimize import minimize  # Import the 'minimize' function
+from sklearn.metrics import mean_squared_error
 
-
-import warnings
-warnings.filterwarnings("ignore", message="X does not have valid feature names")
-
-
-# Load the datasets for machine type #1 and machine type #2
+# Read the first Feather file (machine1.feather)
 data_machine1 = pd.read_feather('machine1.feather')
+
+# Read the second Feather file (machine2.feather)
 data_machine2 = pd.read_feather('machine2.feather')
 
-# Filter out rows with 'check' value between 90 and 110
-data_machine1 = data_machine1[(data_machine1['check'] >= 90) & (data_machine1['check'] <= 110)]
-data_machine2 = data_machine2[(data_machine2['check'] >= 90) & (data_machine2['check'] <= 110)]
+# Print data
+print(data_machine1)
+print(data_machine2)
 
-# Split the data into training and testing sets
-X_train1, X_test1, y_train1, y_test1 = train_test_split(data_machine1[['input_1', 'input_2', 'input_3']], data_machine1['power'], test_size=0.2, random_state=42)
-X_train2, X_test2, y_train2, y_test2 = train_test_split(data_machine2[['input_1', 'input_2', 'input_3']], data_machine2['power'], test_size=0.2, random_state=42)
+# Filter data for machine type #1
+data_machine1_filtered = data_machine1[(data_machine1['check'] >= 90) & (data_machine1['check'] <= 110)]
 
-# Create and train models for each machine type
-model1 = LinearRegression()
-model1.fit(X_train1, y_train1)
+# Filter data for machine type #2
+data_machine2_filtered = data_machine2[(data_machine2['check'] >= 90) & (data_machine2['check'] <= 110)]
 
-model2 = RandomForestRegressor()
-model2.fit(X_train2, y_train2)
 
-# Predict power for the test set
-y_pred1 = model1.predict(X_test1)
-y_pred2 = model2.predict(X_test2)
 
-# Calculate evaluation metrics
-mae1 = mean_absolute_error(y_test1, y_pred1)
-mae2 = mean_absolute_error(y_test2, y_pred2)
+def train_model(data, machine_type):
+    X = data[['input_1', 'input_2', 'input_3']]
+    y = data['power']
 
-mse1 = mean_squared_error(y_test1, y_pred1)
-mse2 = mean_squared_error(y_test2, y_pred2)
+    # Split the data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-print(f"Machine type #1 - MAE: {mae1}, MSE: {mse1}")
-print(f"Machine type #2 - MAE: {mae2}, MSE: {mse2}")
+    # Train a linear regression model
+    model = LinearRegression()
+    model.fit(X_train, y_train)
 
-# Define Objective Function for Optimization
-def total_power(gph_values):
-    # Calculate power consumption for each machine based on GPH values and models
-    power1 = model1.predict([[25, 6, gph] for gph in gph_values[:10]])
-    power2 = model2.predict([[25, 6, gph] for gph in gph_values[10:]])
+    # Predict power usage for the test set
+    y_pred = model.predict(X_test)
 
-    # Calculate total power consumption of the factory
-    total_power = sum(power1) + sum(power2)
+    # Calculate Mean Squared Error as a metric of accuracy
+    mse = mean_squared_error(y_test, y_pred)
+
+    # Plot the results
+    plt.scatter(y_test, y_pred)
+    plt.xlabel('Actual Power')
+    plt.ylabel('Predicted Power')
+    plt.title(f'Machine Type #{machine_type} - Power Prediction (MSE: {mse:.2f})')
+    plt.show()
+
+    return model, mse
+
+
+# Train model for machine type #1
+model_machine1, mse_machine1 = train_model(data_machine1_filtered, 1)
+
+# Train model for machine type #2
+model_machine2, mse_machine2 = train_model(data_machine2_filtered, 2)
+
+# Print the MSE for each machine type
+print(f'MSE for Machine Type #1: {mse_machine1:.2f}')
+print(f'MSE for Machine Type #2: {mse_machine2:.2f}')
+
+
+# Define the target total GPH for the factory
+target_total_GPH = 9000
+
+# Define the GPH range for each machine type
+GPH_range_machine1 = range(400, 426, 1)  # Reducing the range from 180-600 to 400-425 with a step of 1
+GPH_range_machine2 = range(625, 676, 1)  # Reducing the range from 300-1000 to 625-675 with a step of 1
+
+
+# Function to calculate total power for a given GPH configuration
+def calculate_total_power(model1, model2, GPH_values_machine1, GPH_values_machine2):
+    # Convert numpy arrays to DataFrames with correct column names
+    df_machine1 = pd.DataFrame({'input_1': 25, 'input_2': 6, 'input_3': GPH_values_machine1})
+    df_machine2 = pd.DataFrame({'input_1': 25, 'input_2': 6, 'input_3': GPH_values_machine2})
+
+    # Calculate power for each machine using the trained models
+    power_machine1 = model1.predict(df_machine1)
+    power_machine2 = model2.predict(df_machine2)
+
+    # Calculate total power for all machines
+    total_power = sum(power_machine1) + sum(power_machine2)
+
     return total_power
 
-# Set GPH limits for each machine type
-gph_limits = [(180, 600)] * 10 + [(300, 1000)] * 10
+# Initialize variables to store the optimal GPH values and total power
+optimal_GPH_machine1 = 0
+optimal_GPH_machine2 = 0
+lowest_total_power = float('inf')
 
-# Set target total GPH for the factory
-target_total_gph = 9000
+# Iterate through all possible GPH values for each machine type and find the optimal configuration
+for GPH_machine1 in range(180, 601):
+    for GPH_machine2 in range(300, 1001):
+        total_power = calculate_total_power(model_machine1, model_machine2, [GPH_machine1] * 10, [GPH_machine2] * 10)
+        if total_power < lowest_total_power and (GPH_machine1 * 10 + GPH_machine2 * 10) == target_total_GPH:
+            lowest_total_power = total_power
+            optimal_GPH_machine1 = GPH_machine1
+            optimal_GPH_machine2 = GPH_machine2
 
-# Define the objective function for optimization
-def objective_function(gph_values):
-    return total_power(gph_values)
+# Print the optimal GPH values and total power
+print(f'Optimal GPH for Machine Type #1: {optimal_GPH_machine1}')
+print(f'Optimal GPH for Machine Type #2: {optimal_GPH_machine2}')
+print(f'Total Power: {lowest_total_power:.2f}')
 
-# Perform optimization
-result = minimize(objective_function, [300] * 20, bounds=gph_limits, constraints={'type': 'eq', 'fun': lambda x: sum(x) - target_total_gph})
-optimal_gph_values = result.x
+# Optimization evaluation
+
+# Calculate total power consumption before optimization
+total_power_before_optimization = calculate_total_power(model_machine1, model_machine2, [400] * 10, [625] * 10)
+
+# Calculate total power consumption after optimization
+total_power_after_optimization = calculate_total_power(model_machine1, model_machine2, [optimal_GPH_machine1] * 10, [optimal_GPH_machine2] * 10)
+
+# Print the results
+print(f'Total Power Consumption Before Optimization: {total_power_before_optimization:.2f}')
+print(f'Total Power Consumption After Optimization: {total_power_after_optimization:.2f}')
+
+# Calculate power savings
+power_savings = total_power_before_optimization - total_power_after_optimization
+print(f'Power Savings: {power_savings:.2f}')
+
 
 # Save evaluation results to output file
 with open('evaluation_results.txt', 'w') as f:
-    f.write(f"Machine type #1 - MAE: {mae1}, MSE: {mse1}\n")
-    f.write(f"Machine type #2 - MAE: {mae2}, MSE: {mse2}\n")
+    f.write(f"Machine type #1 - MSE: {mse_machine1:.2f}\n")
+    f.write(f"Machine type #2 - MSE: {mse_machine2:.2f}\n")
 
 # Save optimization results to output file
 with open('optimization_results.txt', 'w') as f:
     for i in range(20):
-        f.write(f"Machine {i+1} - GPH: {optimal_gph_values[i]}\n")
-    total_power_consumption = total_power(optimal_gph_values)
-    f.write(f"Total Power Consumption: {total_power_consumption}\n")
+        f.write(f"Machine {i+1} - GPH: {optimal_GPH_machine1 if i < 10 else optimal_GPH_machine2}\n")
+    total_power_consumption = calculate_total_power(model_machine1, model_machine2, [optimal_GPH_machine1] * 10, [optimal_GPH_machine2] * 10)
+    f.write(f"Total Power Consumption: {total_power_consumption:.2f}\n")
